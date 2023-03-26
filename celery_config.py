9e798b774e77aebff1f4123fc2e5a1b6fb392f15
kaucapbm94 <48528021+kaucapbm94@users.noreply.github.com>
2023-03-26 23:35:15 +0600

    asd

--- celery_config.py
+++ celery_config.py
@@ -4,10 +4,29 @@ env = environ.Env()
 environ.Env.read_env()
 
 # CELERY
-RABBITMQ_USER = env('RABBITMQ_USER', default='guest')
-RABBITMQ_PASSWORD = env('RABBITMQ_PASSWORD', default='guest')
-RABBITMQ_HOST = env('RABBITMQ_HOST', default='127.0.0.1:5672')
+# RABBITMQ_USER = env('RABBITMQ_USER', default='guest')
+# RABBITMQ_PASSWORD = env('RABBITMQ_PASSWORD', default='guest')
+# RABBITMQ_HOST = env('RABBITMQ_HOST', default='127.0.0.1:5672')
 
+RABBITMQ_PASSWORD = env('RABBITMQ_PASSWORD', default='7820')
+RABBITMQ_USER = env('RABBITMQ_USER', default='Kaisar')
+RABBITMQ_HOST = env('RABBITMQ_HOST', default='185.121.83.108:30031')
+
+
+
+broker_url = f"pyamqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}/"
+task_acks_late = True
+worker_prefetch_multiplier = 1
+celeryd_concurrency=1
+concurrency=1
+# heartbeat = 60
+broker_heartbeat = 0
+
+# new
+task_time_limit = 3600
+worker_send_task_event = False
+broker_transport_options = {'visibility_timeout': 3600}
+broker_connection_timout = 3600
 # CELERY_BROKER_HEARTBEAT = 10
 # CELERY_BROKER_CONNECTION_TIMEOUT = 30
 # CELERY_WORKER_CONCURRENCY = 50
@@ -23,13 +42,6 @@ RABBITMQ_HOST = env('RABBITMQ_HOST', default='127.0.0.1:5672')
 # CELERY_EVENT_QUEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
 # CELERY_WORKER_PREFETCH_MULTIPLIER = 1 # Disable prefetching, it's causes problems and doesn't help performance
 
-broker_url = f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}/"
-task_acks_late = True
-worker_prefetch_multiplier = 1
-celeryd_concurrency=1
-concurrency=1
-heartbeat = 60
-broker_heartbeat = 60
 # broker_heartbeat = 3600
 # heartbeat = 3600
 # worker_send_task_event = False
@@ -39,7 +51,6 @@ broker_heartbeat = 60
 # task will raise exception SoftTimeLimitExceeded after 50 seconds
 # task_soft_time_limit = 3600
 # task messages will be acknowledged after the task has been executed, not just before (the default behavior).
-# task_acks_late = True
 # One worker taks 10 tasks from queue at a time and will increase the performance
 # worker_prefetch_multiplier = 1
 # worker_concurrency=1
